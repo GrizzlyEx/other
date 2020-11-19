7f562f63c19f6240ac95cc19cfa00d321b996791
GrizzlyEx <nastya6633@bk.ru>
2020-11-19 16:32:20 +0500

    Added provision of task and beginning of writing

--- interview test task.py
+++ interview test task.py
@@ -9,10 +9,44 @@
 из транспортных средств (Например ‘Самокат: вес - 12.4 кг, цвет – красный’).
 
 Свойства транспортных средств:
-Самокат – вес, цвет.
-Велосипед – вес, цвет, количество передач.
-Электросамокат - вес, цвет, мощность электромотора.
-Мотоцикл – вес, цвет, мощность мотора, время разгона до 100 км/ч.
-Автомобиль – вес, цвет, мощность мотора, время разгона до 100 км/ч,
-                                    расположение руля(правое/левое).
+Самокат 'Kick scooter' (KS) – вес, цвет.
+Велосипед 'Bicycle' – вес, цвет, количество передач.
+Электросамокат 'EKS' - вес, цвет, мощность электромотора.
+Мотоцикл 'Motorcycle' (Moto) – вес, цвет, мощность мотора, время разгона до 100 км/ч.
+Автомобиль 'Car' – вес, цвет, мощность мотора, время разгона до 100 км/ч,
+                                        расположение руля(правое/левое).
 """
+
+from random import randint
+
+
+class KS():
+    def weight(self):
+        self.weight = randint(5, 15)
+        return self.weight
+
+    def color(self):
+        color_list = [
+            'White', 'Black', 'Brown',
+            'Blue', 'Gray', 'Red', 'Orange',
+            'Yellow', 'Purple', 'Pink']
+        self.color = color_list[randint(0, 9)]
+        return self.color
+
+
+class Bicycle(KS):
+    def __init__(self):
+        self.transmission = randint(1, 20)
+        self.color = KS().color()
+
+
+class EKS(KS):
+    def __init__(self):
+        self.weight = KS().weight()
+        self.power = randint(180, 480)
+        self.color = KS().color()
+
+Samo = KS()
+print(Samo.color(), Samo.weight())
+Yamaha = EKS()
+print(Yamaha.power, Yamaha.color)
